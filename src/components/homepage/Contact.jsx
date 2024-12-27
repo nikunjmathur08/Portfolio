af1b9f358e1dea76797ffc57b5df1c6c9c1cdfda
Nikunj Mathur <nikunjmathur0810@gmail.com>
2024-12-27 09:01:34 +0530

    feat: add resume link

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -135,7 +135,7 @@ export default function Contact() {
               <a
                 href="https://drive.google.com/file/d/1ej8iI6Q0j4JHG4oBUCrMAhhzcwoE3Hnb/view?usp=sharing"
                 className="group relative w-fit cursor-pointer"
-                target=""
+                target="_blank"
                 rel="noreferrer"
               >
                 <span>Resume</span>
