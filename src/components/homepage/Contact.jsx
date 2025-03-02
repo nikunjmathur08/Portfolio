ff28f439f81c99cb70148d73dbbf87d472689898
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-03-02 10:22:12 +0530

    feat: add new resume link

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -133,7 +133,7 @@ export default function Contact() {
                 <span className="absolute bottom-0 left-0 h-[0.12em] w-0 rounded-full bg-secondary-600 duration-300 ease-in-out group-hover:w-full"></span>
               </a>
               <a
-                href="https://drive.google.com/file/d/1ej8iI6Q0j4JHG4oBUCrMAhhzcwoE3Hnb/view?usp=sharing"
+                href="https://drive.google.com/file/d/1YMdARoVCrD_vTikqp4ZT76PNWrwMGlpO/view?usp=sharing"
                 className="group relative w-fit cursor-pointer"
                 target="_blank"
                 rel="noreferrer"
