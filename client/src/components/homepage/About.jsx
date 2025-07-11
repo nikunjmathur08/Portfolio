7267a60eb8d297bd00032037c762f00b99a10aa5
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-07-11 19:26:09 +0530

    feat: improve alt text for image

--- client/src/components/homepage/About.jsx
+++ client/src/components/homepage/About.jsx
@@ -45,7 +45,7 @@ export default function About() {
             src={profileImg}
             width="600"
             height="800"
-            alt="portrait image of Nikunj standing at Nandi Hills, Bangalore"
+            alt="portrait image of Nikunj posing at an event"
           />
         </div>
         <div className="top-20 sm:sticky md:top-28 lg:top-32 md:w-1/2">
